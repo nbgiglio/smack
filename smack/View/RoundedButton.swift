0f1d16cc6099cf962fa11a4abe7c55e1b9460050
Freedom Suite <freedomsuite@Freedoms-MacBook-Pro.local>
2018-10-12 11:11:20 -0400

    lesson 10

---
+++ smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  smack
+//
+//  Created by Freedom Suite on 10/12/18.
+//  Copyright © 2018 Freedom Suite. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView(){
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
