031a88f08d6e6f8461682caf87f2855ac5e1bb75
Freedom Suite <freedomsuite@Freedoms-MacBook-Pro.local>
2018-10-12 15:21:37 -0400

    lesson 13

--- smack/Controller/AvatarPickerVC.swift
+++ smack/Controller/AvatarPickerVC.swift
@@ -14,6 +14,9 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    //Variables
+    var avatarType = AvatarType.dark
+    
     
     
     override func viewDidLoad() {
@@ -24,10 +27,34 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell{
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
         return AvatarCell()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numOfColumns : CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numOfColumns = 4
+        }
+        
+        let spaceBetweenCells : CGFloat = 10
+        let padding : CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark{
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -40,7 +67,18 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
     
     //Actions
     @IBAction func segmentControlChanged(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = AvatarType.dark
+        } else if segmentControl.selectedSegmentIndex == 1 {
+            avatarType = AvatarType.light
+        } else {
+            print("Unknown")
+        }
+        collectionView.reloadData()
     }
+    
+    
+    
     
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -25,9 +25,15 @@ class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
+    
     // IB Actions
     @IBAction func createAccntPressed(_ sender: Any) {
         guard let name = usernameTxt.text , usernameTxt.text != "" else {return}
