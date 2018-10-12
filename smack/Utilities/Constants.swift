8593b7f4d7ebcffc408027f9d13013a1e81cafbe
Freedom Suite <freedomsuite@Freedoms-MacBook-Pro.local>
2018-10-12 13:50:30 -0400

    lesson 12

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -13,6 +13,8 @@ typealias  CompletionHandler = (_ Success: Bool) -> ()
 //URL Constants
 let BASE_URL = "https://chattychatchta.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
 
 
 // Segues
@@ -25,4 +27,7 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
-
+// Headers
+let HEADER = [
+    "Content-Type":"application/json; charset=utf-8"
+]
